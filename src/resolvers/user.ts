2a4f7b216c8053ec8f30fd00c1c0223b4c8cd23d
Samith Dilhara <h.samithdilhara@gmail.com>
2020-09-12 13:48:40 +0530

    finishing off server setup

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -5,7 +5,8 @@ import {
   Field,
   Arg,
   Ctx,
-  ObjectType
+  ObjectType,
+  Query
 } from 'type-graphql'
 import { MyContext } from 'src/types'
 import { User } from '../entities/User'
@@ -38,10 +39,21 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // you are not logged in
+    if (!req.session.userId) {
+      return null
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId })
+    return user
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('inputs') inputs: Inputs,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (inputs.username.length <= 2) {
       return {
@@ -86,13 +98,20 @@ export class UserResolver {
         }
       }
     }
+
+    // optional
+    // store user id session
+    // this will set a cookie on the user
+    // keep them logged in
+    req.session.userId = user.id
+
     return { user }
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg('inputs') inputs: Inputs,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: inputs.username })
 
@@ -118,6 +137,7 @@ export class UserResolver {
         ]
       }
     }
+    req.session.userId = user.id
 
     return { user }
   }
